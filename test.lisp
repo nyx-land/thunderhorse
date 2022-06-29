f519bb83d6b7fd266eea938a30bdc062e9c50823
Nyx <n1x@riseup.net>
2022-06-28 18:55:12 -0700

    move tests to their own file

---
+++ test.lisp
@@ -0,0 +1,48 @@
+;; TODO: make real rests and not whatever this is
+
+(defparameter *test-heading* (format nil "* TODO [#A] heading :tag1:tag2:~%** heading2~%*** heading3"))
+
+(defparameter *test-paragraphs*
+   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dicam de
+voluptate, nihil scilicet novi, ea tamen, quae te ipsum probaturum esse
+confidam. Traditur, inquit, ab Epicuro ratio neglegendi doloris. Duo Reges:
+constructio interrete. At ille pellit, qui permulcet sensum voluptate. Nec vero
+alia sunt quaerenda contra Carneadeam illam sententiam. Dicam, inquam, et quidem
+discendi causa magis, quam quo te aut Epicurum reprehensum velim. Si verbum
+sequimur, primum longius verbum praepositum quam bonum. Illud quaero, quid ei,
+qui in voluptate summum bonum ponat, consentaneum sit dicere. Quid enim necesse
+est, tamquam meretricem in matronarum coetum, sic voluptatem in virtutum
+concilium adducere? Aliter enim nosmet ipsos nosse non possumus.
+
+Ut proverbia non nulla veriora sint quam vestra dogmata. Ita nemo beato beatior.
+Qui enim existimabit posse se miserum esse beatus non erit. Non enim, si omnia
+non sequebatur, idcirco non erat ortus illinc. An, partus ancillae sitne in
+fructu habendus, disseretur inter principes civitatis, P. Miserum hominem! Si
+dolor summum malum est, dici aliter non potest. Respondeat totidem verbis.
+Quonam modo? An est aliquid per se ipsum flagitiosum, etiamsi nulla comitetur
+infamia? Te ipsum, dignissimum maioribus tuis, voluptasne induxit, ut
+adolescentulus eriperes P.
+
+Consequentia exquirere, quoad sit id, quod volumus, effectum. Quam ob rem
+tandem, inquit, non satisfacit? Sed nimis multa. Prioris generis est docilitas,
+memoria; Sed quoniam et advesperascit et mihi ad villam revertendum est, nunc
+quidem hactenus; Quid enim de amicitia statueris utilitatis causa expetenda
+vides. Illud non continuo, ut aeque incontentae. Quid ergo attinet gloriose
+loqui, nisi constanter loquare? Vos autem cum perspicuis dubia debeatis
+illustrare, dubiis perspicua conamini tollere.
+
+Honesta oratio, Socratica, Platonis etiam. Hoc ipsum elegantius poni meliusque
+potuit. An, partus ancillae sitne in fructu habendus, disseretur inter principes
+civitatis, P. Hic Speusippus, hic Xenocrates, hic eius auditor Polemo, cuius
+illa ipsa sessio fuit, quam videmus. Cum ageremus, inquit, vitae beatum et
+eundem supremum diem, scribebamus haec.
+
+Mihi vero, inquit, placet agi subtilius et, ut ipse dixisti, pressius. Ac ne
+plura complectar-sunt enim innumerabilia-, bene laudata virtus voluptatis aditus
+intercludat necesse est. Nam ista vestra: Si gravis, brevis; Id quaeris, inquam,
+in quo, utrum respondero, verses te huc atque illuc necesse est. Immo vero,
+inquit, ad beatissime vivendum parum est, ad beate vero satis. Quae
+diligentissime contra Aristonem dicuntur a Chryippo. Similiter sensus, cum
+accessit ad naturam, tuetur illam quidem, sed etiam se tuetur; Semper enim ex
+eo, quod maximas partes continet latissimeque funditur, tota res appellatur. An
+est aliquid, quod te sua sponte delectet? Paria sunt igitur.")
